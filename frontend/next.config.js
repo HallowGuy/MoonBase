62c2402a791b4fa976d5ce7795a24a65d882cc21
Hadi ABDALLAH <hadi.abdallah@intalio.com>
2025-06-09 14:53:22 +0200

    changed manually

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  basePath: '/app',
+  assetPrefix: '/app',
   experimental: {
     appDir: true
   }
 };
+
 module.exports = nextConfig;
